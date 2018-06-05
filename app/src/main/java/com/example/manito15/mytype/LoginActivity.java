197612e07a83af343518c69b05f12a18ff41fd38
gusrud1103 <1995tls@naver.com>
2018-06-06 03:12:44 +0900

    현경 commit: 디자인 개선(로딩, 로그인, 검색화면)

--- app/src/main/java/com/example/manito15/mytype/LoginActivity.java
+++ app/src/main/java/com/example/manito15/mytype/LoginActivity.java
@@ -1,19 +1,15 @@
 package com.example.manito15.mytype;
 
 import android.content.Intent;
-
+import android.graphics.PorterDuff;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.LinearLayout;
 import android.widget.Toast;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
-
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -30,16 +26,15 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity  implements GoogleApiClient.OnConnectionFailedListener{
 
-
     private static final int RC_SIGN_IN = 10;
     private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
-
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -59,8 +54,6 @@ public class LoginActivity extends AppCompatActivity  implements GoogleApiClient
                 startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
-
-
 
 
     }
