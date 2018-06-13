e9d8e0ec22a74dd9a3d8e90619da8b0aba86e56d
gusrud1103 <1995tls@naver.com>
2018-06-13 17:49:16 +0900

    현경 commit: 일단임시방편

---
+++ app/src/main/java/com/example/manito15/mytype/fragment/HomeDTO.java
@@ -0,0 +1,14 @@
+package com.example.manito15.mytype.fragment;
+
+/**
+ * Created by LG on 2018-06-13.
+ */
+
+public class HomeDTO {
+    public String edt_review_write;
+    public String imageUrl;
+    public String userId;
+
+
+
+}
--- app/src/main/java/com/example/manito15/mytype/fragment/HomeFragment.java
+++ app/src/main/java/com/example/manito15/mytype/fragment/HomeFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,17 +38,16 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     View v;
     Context context;
-    int memberId;
 
     RecyclerView recyclerView;
     LinearLayoutManager linearLayoutManager;
-    TextView noDataText;
+    //TextView noDataText;
 
     //ReviewListAdapter reviewListAdapter;
     ListAdapter listAdapter;
-    List<ReviewItem> lstReview;
+    //List<ReviewItem> lstReview;
 
-    private List<ImageDTO> imageDTOs = new ArrayList<>();
+    private List<HomeDTO> imageDTOs = new ArrayList<>(); //ImageDTO
     private List<String> uidLists = new ArrayList<>();
     private FirebaseDatabase database;
 
@@ -58,7 +58,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -66,43 +65,46 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 //        memberId = ((MyApp)this.getActivity().getApplicationContext()).getMemberId();
 
         v =  inflater.inflate(R.layout.fragment_home, container, false);
-
         recyclerView = (RecyclerView) v.findViewById(R.id.recyclerview); //리싸이클러뷰 가져옴
 
-        database = FirebaseDatabase.getInstance();
-
-
-        linearLayoutManager = new LinearLayoutManager(context);
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        //linearLayoutManager.setReverseLayout(true);
+        //linearLayoutManager.setStackFromEnd(true);
         //reviewListAdapter = new ReviewListAdapter(context, R.layout.row_review_list, new ArrayList<ReviewItem>());
         //recyclerView.setAdapter(reviewListAdapter);
+        database = FirebaseDatabase.getInstance();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
         listAdapter = new ListAdapter();
         recyclerView.setAdapter(listAdapter);
-        return v;
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        img_like=(CheckBox) v.findViewById(R.id.img_like);
-        img_like.setOnClickListener(this);
-        database.getReference().child("images").addValueEventListener(new ValueEventListener() {
+        //database.getReference().child("images").addValueEventListener(new ValueEventListener() {
+        //    @Override
+        //    public void onDataChange(DataSnapshot dataSnapshot) {
+
+        //        imageDTOs.clear();;
+                //for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                //     HomeDTO imageDTO = snapshot.getValue(HomeDTO.class);//ImageDTO
+                //     imageDTOs.add(imageDTO);
+                //}
+
+        //        listAdapter.notifyDataSetChanged();
+        //        String value = dataSnapshot.getValue(String.class);
+        //        // Log.d(TAG, "Value is: " + value);
+
+        //    }
+
+        //    @Override
+        //    public void onCancelled(DatabaseError databaseError) {
+        //        // Log.w(TAG, "Failed to read value.", databaseError.toException());
+        //    }
+        //});
+        FirebaseDatabase.getInstance().getReference().addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                imageDTOs.clear();;
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    ImageDTO imageDTO = snapshot.getValue(ImageDTO.class);
-                    imageDTOs.add(imageDTO);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Log.d("MainActivity", "Single ValueEventListener : " + snapshot.getValue());
                 }
-                listAdapter.notifyDataSetChanged();
-
-
-
-
             }
 
             @Override
@@ -113,12 +115,23 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
 
 
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //img_like=(CheckBox) v.findViewById(R.id.img_like);
+        //img_like.setOnClickListener(this);
 
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         //lstReview = new ArrayList<>();
         //lstReview.add(new ReviewItem("고아라1"));
@@ -130,19 +143,19 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()) {
-            case R.id.img_like:
-                if (img_like.isChecked()) {
+        //switch (v.getId()) {
+            //case R.id.img_like:
+                //if (img_like.isChecked()) {
                     // TODO : CheckBox is checked.
-                    img_like.setButtonDrawable(R.drawable.ic_favorite_on_black_24dp);
-                    like="good";
-                } else {
+                    //img_like.setButtonDrawable(R.drawable.ic_favorite_on_black_24dp);
+                    //like="good";
+                //} else {
                     // TODO : CheckBox is unchecked.
-                    img_like.setButtonDrawable(R.drawable.ic_favorite_off_black_24dp);
-                    like=null;
-                }
-                break;
-        }
+                    //img_like.setButtonDrawable(R.drawable.ic_favorite_off_black_24dp);
+                    //like=null;
+                //}
+                //break;
+        //}
     }
     class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
@@ -156,11 +169,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
         @Override
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            ((CustomViewHolder)holder).txt_name.setText(imageDTOs.get(position).userId);
-            ((CustomViewHolder)holder).txt_age.setText(imageDTOs.get(position).edt_price);
-
+            ((CustomViewHolder)holder).txt_age.setText(imageDTOs.get(position).edt_review_write);
             Glide.with(holder.itemView.getContext()).load(imageDTOs.get(position).imageUrl).into(((CustomViewHolder)holder).imageView);
-
+            ((CustomViewHolder)holder).txt_name.setText(imageDTOs.get(position).userId);
 
         }
 
@@ -169,18 +180,19 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             return imageDTOs.size();
         }
 
+
         private class CustomViewHolder extends RecyclerView.ViewHolder {
             ImageView imageView;
             TextView txt_name;
             TextView txt_age;
-            TextView txt_age_range;
+
 
             public CustomViewHolder(View view) {
                 super(view);
                 imageView = (ImageView) view.findViewById(R.id.img_review);
                 txt_name = (TextView) view.findViewById(R.id.txt_name);
                 txt_age = (TextView) view.findViewById(R.id.txt_age);
-                txt_age_range= (TextView) view.findViewById(R.id.txt_age_range);
+
             }
         }
     }
