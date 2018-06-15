258c99375d5d9216c919e72abcf1dae0743737ad
gusrud1103 <1995tls@naver.com>
2018-06-15 10:11:26 +0900

    현경 commit: 불필요한 Permission제거

--- app/src/main/java/com/example/manito15/mytype/PermissionActivity.java
+++ app/src/main/java/com/example/manito15/mytype/PermissionActivity.java
@@ -53,8 +53,7 @@ public class PermissionActivity extends AppCompatActivity {
      */
     private boolean checkAndRequestPermissions(){
         String[] permissions = new String[]{
-                Manifest.permission.CAMERA,
-                Manifest.permission.READ_PHONE_STATE,
+
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.READ_EXTERNAL_STORAGE
         };
